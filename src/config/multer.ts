10aa735e0eefaeb4b86856c16538faf6fcd75883
GuilhermeVDG <guivdouradog@gmail.com>
2025-05-21 12:02:25 -0300

    refactor multer configuration to use temporary directory for file uploads

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,12 +1,13 @@
 import crypto from 'crypto';
 import multer from 'multer';
-import { extname, resolve } from 'path';
+import path, { extname, resolve } from 'path';
+import os from 'os';
 
 export default {
   upload(folder: string){
     return{
       storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', folder),
+        destination: path.join(os.tmpdir()),
         filename: (requst, file, callback) => {
           const fileHash = crypto.randomBytes(16).toString("hex");
           const fileName = `${fileHash}-${file.originalname}`;
